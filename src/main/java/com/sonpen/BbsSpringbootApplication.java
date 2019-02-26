bae28e90b030cabf0e3d735c165b2e311ca5b351
sonpen <sopnen76@naver.com>
2019-02-26 17:01:40 +0900

    Initial commit

---
+++ src/main/java/com/sonpen/BbsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.sonpen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbsSpringbootApplication.class, args);
+	}
+
+}
